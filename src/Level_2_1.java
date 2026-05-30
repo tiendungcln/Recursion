d4aaafb3ec78b6c8cb06dc818c53702949f89bdd
tiendungcln <tiendung0301cln@gmail.com>
2026-05-30 16:28:14 +0700

    add Level_2_1

---
+++ src/Level_2_1.java
@@ -0,0 +1,29 @@
+public class Level_2_1 {
+    // Tính tổng số chẵn
+    // Ví dụ:
+    // n = 6
+    // => 2 + 4 + 6 = 12
+
+    public static int sumEven(int n){
+
+        // base case
+        if (n == 0){
+            return 0;
+        }
+
+        if (n % 2 == 0){
+            return n + sumEven(n - 2);
+        }
+
+        return sumEven(n - 1);
+
+    }
+
+    public static void main(String[] args){
+
+        int n = 7;
+        System.out.println(sumEven(n));
+
+    }
+
+}
